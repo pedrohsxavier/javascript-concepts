5f3f81aec754732edd5221eb0d3e5cf54bda8175
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-06-22 00:02:25 -0300

    functions

---
+++ JavaScript-Functions.js
@@ -0,0 +1,23 @@
+// Media Function with three parameters
+function media(x, y, z) {
+  return (x + y + z) / 3
+}
+
+//Using Fuction
+result = media(10.0, 10.0, 9.8);
+console.log(result);
+
+//Function without parameters
+function helloWorld() {
+  return "Hello World!";
+}
+
+//Hello World!
+console.log(helloWorld());
+
+//Parameters without type
+function square(number) {
+  return number * number;
+}
+
+console.log(square(5));
