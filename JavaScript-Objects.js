8a192dc4f9b2f063f56552be1c26761ac8d6cff5
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-06-22 00:18:13 -0300

    objects

---
+++ JavaScript-Objects.js
@@ -0,0 +1,12 @@
+var car = {
+  nome: "Cruze",
+  fabricante: "Chevrolet"
+};
+
+//Getting keys
+console.log("Nome: " + car.nome)
+console.log("Fabricante: " + car.fabricante)
+
+//Setting keys
+car.nome = "Camaro";
+console.log("Novo carro: " + car.nome.toUpperCase());
