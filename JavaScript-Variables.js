1fa27ffb9db9accaddea3fbbb88ef3a8a4da10de
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-06-21 23:59:52 -0300

    variables

---
+++ JavaScript-Variables.js
@@ -0,0 +1,14 @@
+/*
+var -> Keyword that tells JavaScript for declare a variable
+num -> Name of that variable
+=   -> Assignment Operator 
+20  -> Value stored
+*/
+var num = 20;
+
+num = num * num;
+console.log(num);
+
+//Reassigning variables
+num = 600;
+console.log(num);
