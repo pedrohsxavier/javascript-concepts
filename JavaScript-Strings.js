5bdd5bca705dc69fbb5318bc9af23e974c94dbe3
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-06-21 23:53:13 -0300

    strings

---
+++ JavaScript-Strings.js
@@ -0,0 +1,25 @@
+let word1 = 'This is a string!';
+let word2 = "This is the 2nd string.";
+let word3 = "It's six o'clock.";
+let word4 = 'Remember to say "please" and "thank you"';
+let word5 = "Remember to say \"please\" and \"thank you\"";
+
+console.log(word1)
+console.log(word2)
+console.log(word3)
+console.log(word4)
+console.log(word5)
+console.log(word1.length)
+console.log(word2.length)
+console.log(word3.length)
+console.log(word4.length)
+console.log(word5.length)
+
+let kids = "THE KIDS".toLowerCase();
+console.log(kids)
+
+ kids = kids.toUpperCase();
+console.log(kids);
+
+let word6 = "    Words    ";
+console.log(word6.trim());
