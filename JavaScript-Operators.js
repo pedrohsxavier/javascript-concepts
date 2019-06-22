8e6373135711763a25b96b16059d4087052354a4
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-06-21 23:58:10 -0300

    operators

---
+++ JavaScript-Operators.js
@@ -0,0 +1,32 @@
+// (+) Operator
+var a = 15 + 20;
+console.log(a);
+
+// (-) Operator
+var b = 60 - 25;
+console.log(b)
+
+// (*) Operator
+var c = 42 * 8;
+console.log(c);
+
+// (/) Operator
+var d = 40 / 5;
+console.log(d);
+
+//Order of operations
+var e = 5 + 8 * 2 - 10 / 2;
+//Answer: 16
+console.log(e);
+
+//Grouping
+var f = (5 + 2) * 4 - 2 + (5 + 3);
+//Answer: 34
+console.log(f);
+
+// (+) Concatenation
+console.log("Over" + "flow");
+
+// (=) Assignment
+var car = "Ferrari";
+console.log(car)
